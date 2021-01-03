e1cb7af9dc849a9695eae520df96011caa05be3b
CyberDrudge <satyamsaxena197@gmail.com>
2021-01-03 14:57:51 +0530

    Update API Url

--- server.js
+++ server.js
@@ -13,8 +13,5 @@ app.get('/*', (req, res) => {
 });
 // Start the app by listening on the default Heroku port
 // app.listen(port);
-app.listen(port, () => {
-   console.log('Server is up!');
-   console.log(publicPath)
-});
+app.listen(port);
 
